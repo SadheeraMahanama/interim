92520306d2f04fe78bc13ad3f80fd747044e7de6
SadheeraSMahanama <sadheeramahanama94@gmail.com>
2018-12-15 00:13:38 +0530

    AboutApp

--- Components/About.js
+++ Components/About.js
@@ -1,9 +1,10 @@
  
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,Image} from 'react-native';
-import { Icon, Button, Container, Header, Content, Left } from 'native-base'
+import {Platform, StyleSheet, Text, View,Image,Button} from 'react-native';
+import { Icon, Container, Header, Content, Left } from 'native-base'
 import { Dialog } from 'react-native-simple-dialogs'; 
+
 class About extends Component {
     
   static navigationOptions = {
@@ -13,6 +14,12 @@ class About extends Component {
         source={require('../images/about.png')}
         />
       )
+    }
+
+    state = {}
+    
+    openDialog = (show) => {
+    this.setState({ showDialog: show });
     }
 
   render() {
@@ -28,11 +35,118 @@ class About extends Component {
                </Left>
            </Header>
            <Content>
-               <Text>About</Text>
+           <View style={styles.box}>
+                <Button style={styles.btn}
+                    onPress={ () => this.openDialog(true) }
+                    title="About App"
+                />
+               
+                <Dialog
+                    title="ClzMate"
+                    animationType="fade"
+                    contentStyle={
+                        {
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }
+                    }
+                    onTouchOutside={ () => this.openDialog(false) }
+                    visible={ this.state.showDialog }
+                >
+                <Image
+                    source={require('../images/clz.jpg')}
+                        // source={
+                        //     {
+                        //         uri: "https://facebook.github.io/react-native/img/header_logo.png",
+                        //     }
+                        // }
+                        style={
+                            {
+                                width: 99,
+                                height: 87,
+                                backgroundColor: "white",
+                                marginTop: 10,
+                                resizeMode: "contain",
+                            }
+                        }
+                    />
+                    <Text style={ { marginVertical: 30 } }>
+                         Sadheera Mahanama
+                         sadheeramahanama94@gmail.com
+
+
+                    </Text>
+                    <Button
+                        onPress={ () => this.openDialog(false) }
+                        style={ { marginTop: 10 } }
+                        title="CLOSE"
+                    />
+                </Dialog>
+                </View>
            </Content>
        </Container>
+    // <View>
+                 
+    //             <Button
+    //                 onPress={ () => this.openDialog(true) }
+    //                 title="Custom Dialog"
+    //             />
+
+              
+
+    //             <Dialog
+    //                 title="Custom Dialog"
+    //                 animationType="fade"
+    //                 contentStyle={
+    //                     {
+    //                         alignItems: "center",
+    //                         justifyContent: "center",
+    //                     }
+    //                 }
+    //                 onTouchOutside={ () => this.openDialog(false) }
+    //                 visible={ this.state.showDialog }
+    //             >
+    //                 <Image
+    //                     source={
+    //                         {
+    //                             uri: "https://facebook.github.io/react-native/img/header_logo.png",
+    //                         }
+    //                     }
+    //                     style={
+    //                         {
+    //                             width: 99,
+    //                             height: 87,
+    //                             backgroundColor: "black",
+    //                             marginTop: 10,
+    //                             resizeMode: "contain",
+    //                         }
+    //                     }
+    //                 />
+    //                 <Text style={ { marginVertical: 30 } }>
+    //                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    //                 </Text>
+    //                 <Button
+    //                     onPress={ () => this.openDialog(false) }
+    //                     style={ { marginTop: 10 } }
+    //                     title="CLOSE"
+    //                 />
+    //             </Dialog>
+
+    //              </View>
+                
     );
   }
 }
+
+const styles = StyleSheet.create({
+    btn :{
+         flex:1,
+         justifyContent:'center',
+         alignItems:'center'
+         
+     }
+     
+    
+  });
 
 export default About;
